174cc51cb7f9260fb8b1deefe3326272bc13fd89
Rogério Alonso <69876702+rogerioloualonso@users.noreply.github.com>
2024-07-26 17:56:48 -0300

    Projeto inicial do Spring Boot Starter 🌟

---
+++ src/main/java/com/automatiLife/AutomatiLifeApplication.java
@@ -0,0 +1,13 @@
+package com.automatiLife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatiLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatiLifeApplication.class, args);
+	}
+
+}
